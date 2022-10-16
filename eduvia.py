4b2c288548a4b1fb0f1bbe78bd3180678f573119
Ali Khani <khani@berkeley.edu>
2022-10-16 06:10:22 -0700

    auto clear prereqs, checks prereqs ok now

--- eduvia.py
+++ eduvia.py
@@ -31,53 +31,83 @@ def main():
     for i in range(len(data)):
         if(data[i]['school'] == school):
             deets = data[i]
-
     
+    new_class = ""
+    taken = []
+    while (new_class != "e"):
+        new_class = input("Type a class you've taken (press 'e' to exit): \n")
+        if new_class in deets["classes"]: 
+            taken.append(new_class)
+            
+
     # TODO: ask which course they want to take
     course = input("Which course do you want to take? \n")
 
     #run toposort() on the school with the given course
-    path = toposort(deets['prereqs'], course)
-    
-    # check school types, proceed accordingly
-    if ("school type is HS"): # TODO
-        years = input("How many years (including this one) do you have left? \n")
+    path = (list) (reversed(toposort(deets['prereqs'], course)))
 
-        print(path[:years]) # TODO: make it print actual path
-
-    elif ("school type is UNI"): # TODO
-        sems = input("How many semesters (including this one) do you have left? \n")
+    def assumed_prereqs(took):
+        uptothat = (toposort(deets['prereqs'], took))
+        return [u for u in uptothat if u in path]
         
-        print(path[:sems]) # TODO: make it print actual path
+    for i in range(len(taken)):
+        taken.extend(assumed_prereqs(taken[i]))
+
+    remaining = []
+    for i in range(len(path)):
+        if path[i] not in taken:
+            remaining.append(path[i])
+    
+    def prereq_check(rem, time_left):
+        t = 0
+        for i in range(len(rem) - 1):
+            if rem[i] in deets["prereqs"][rem[i + 1]]:
+                t += 1
+        if t >= time_left:
+            print("You do not have enough time left to take this course.")
+            print("You would need to finish: " + (str) (remaining))
+        else:
+            print("It's doable! Courses needed: " + (str) (remaining))
+
+    # check school types, proceed accordingly
+    if (deets["type"] == "hs"): # TODO
+        years = (int) (input("How many years (including this one) do you have left? \n"))
+        prereq_check(remaining, years)
+
+    else: # TODO
+        sems = (int) (input("How many semesters (including this one) do you have left? \n"))
+        prereq_check(remaining, sems)
 
 
 # graph = {'A':['B','C'],'B':['D','E'],'C':['D','E'],'D':['E'],'E':['A']}
 # print(toposort(graph, 'A'))
 
-courses = {
-        '54': ['1B'], 
-        '53': ['1B'],
-        '1B': ['1A'],
-        '1A': []
-        }
-print(toposort(courses, '54'))
+# courses = {
+#         '54': ['1B'], 
+#         '53': ['1B'],
+#         '1B': ['1A'],
+#         '1A': []
+#         }
+# print(toposort(courses, '54'))
 
-berkeley_cs = {
-    '189': ['70'],
-    '182': ['61B', '70', '189'],
-    '170': ['61B', '70'],
-    '168': ['162', '61B'],
-    '164': ['61B', '61C'],
-    '162': ['61B', '61C', '70'],
-    '161': ['61C', '70'],
-    '70': [],
-    '61C': ['61B'],
-    '61B': ['61A'],
-    '61A': []
-}
+# berkeley_cs = {
+#     '189': ['70'],
+#     '182': ['61B', '70', '189'],
+#     '170': ['61B', '70'],
+#     '168': ['162', '61B'],
+#     '164': ['61B', '61C'],
+#     '162': ['61B', '61C', '70'],
+#     '161': ['61C', '70'],
+#     '70': [],
+#     '61C': ['61B'],
+#     '61B': ['61A'],
+#     '61A': []
+# }
 
-course = input("Which class? \n")
-result = []
-for i in range(4):
-    result.insert(0, toposort(berkeley_cs, course)[i])
-print("CS " + course + ": " + (str)(result))
+# course = input("Which class? \n")
+# result = (list) (reversed(toposort(berkeley_cs, course)))
+# print("CS " + course + ": " + (str)(result))
+
+# print("\n")
+
+main()

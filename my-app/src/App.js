7dc828073f4c48de8515522d7a01672a5f99a672
wahhajmalik <wsmalik@ucdavis.edu>
2022-10-16 04:52:18 -0700

    Updated App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,18 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
 import BasicExample from "./components/Table.jsx";
 import Header from "./components/Navbar.jsx";
 import "./App.css";
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [show, setShow] = useState(false);
   return(
     <>
     <Header></Header>
-    <BasicExample></BasicExample>
+    {show ?
+    <BasicExample></BasicExample> : null}
+    <Button onClick = {() => setShow(show => !show)}>Hi</Button>
     </>
   );
 }

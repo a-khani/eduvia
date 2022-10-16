dfb6870120f21ceaa1d3e5d1a53f3947d9d0a192
Martin Lacsamana <martinlacsamana@berkeley.edu>
2022-10-16 10:19:20 -0700

    idk

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,7 +13,9 @@ function App() {
     <Header></Header>
     <div class="dummy-text">
         <img src="https://d24fkeqntp1r7r.cloudfront.net/wp-content/uploads/2020/11/08041438/Untitled-design-2020-12-08T151337.059.png"></img>
+        <a href="#first-section" class="bounce"></a>
       </div>
+    <div class="martin">Proof of Concept??</div>
     {show ? <BasicExample></BasicExample> : null}
     <Button onClick = {() => setShow(show => !show)}>hi</Button>
     <ChatPopUp></ChatPopUp>

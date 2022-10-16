4526679d57b88cf788894782b1bee87de4002f84
Wahhaj Malik <wahhaj.malik@gmail.com>
2022-10-16 03:51:25 -0700

    added my app folder

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import BasicExample from "./components/Table.jsx";
+import Header from "./components/Navbar.jsx";
+import "./App.css";
+
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <BasicExample></BasicExample>
+    </>
+  );
+}
+
+export default App;
